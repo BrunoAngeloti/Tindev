018a30fc370fad61a409eac7b7b9c3a45a699726
Bruno Pires <bruno.angeloti@terra.com.br>
2021-03-02 11:18:38 -0300

    Aplicação finalizada

--- tindev/src/pages/Login.js
+++ tindev/src/pages/Login.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import AsyncStorage from '@react-native-community/async-storage'
 import { Text, View, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native'
 
 import logo from '../assets/logo.png'
 
-export default function Login() {
+import api from '../services/api'
+
+export default function Login({ navigation }) {
+    const [user, setUser] = useState('')
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if(user){
+                navigation.navigate('Main', { user })
+            }
+        })
+    }, [])
+
+    async function handleLogin(){
+        const response = await api.post('/devs', { username: user })
+
+        const { _id } = response.data
+
+        await AsyncStorage.setItem('user', _id)
+
+        navigation.navigate('Main', { user: _id })
+    }
+
     return (
         <View style={styles.container}>
             <Image style={styles.img} source={logo} />
@@ -14,9 +37,11 @@ export default function Login() {
                 placeholder="Digite seu usuario do Github"
                 placeholderTextColor="#999" 
                 style={styles.input}
+                value={user}
+                onChangeText={setUser}
             />
 
-            <TouchableOpacity style={styles.button} >
+            <TouchableOpacity onPress={handleLogin} style={styles.button} >
                 <Text style={styles.buttonText}>Enviar</Text>
             </TouchableOpacity>
                  
